8eee0c148c40dfb790fdd528df10676ad825cc36
Harshit P G <harshitpg2004@gmail.com>
2024-08-16 22:11:55 +0530

    update

---
+++ seeders/seedQuestions.js
@@ -0,0 +1,38 @@
+// seedQuestions.js
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+const userModel = require("../models/userModel");
+const questionModel = require("../models/questionModel");
+
+mongoose.connect(process.env.CONNECT_STRING);
+
+const questionFilePath = path.join(__dirname, "../seederQuestions.json");
+
+const seedQuestions = async () => {
+  try {
+    const users = await userModel.find({ questionsSeeded: { $ne: true } });
+
+    for (const user of users) {
+      const questions = JSON.parse(fs.readFileSync(questionFilePath, "utf8"));
+      const userQuestions = questions.map((question) => ({
+        user_id: user._id,
+        question: question.question,
+        answer: question.answer,
+        points: question.points,
+        difficultyLevel: question.difficultyLevel,
+        answered: false,
+      }));
+
+      await questionModel.insertMany(userQuestions);
+
+      await userModel.findByIdAndUpdate(user._id, { questionsSeeded: true });
+    }
+  } catch (error) {
+    console.error("Error seeding questions:", error);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+seedQuestions();
